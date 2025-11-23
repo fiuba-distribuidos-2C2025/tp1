ee61c4bc6cb5df1b1fd2bc81c50b3b01b5b2b4f2
avilagaston9 <gaston.avila@lambdaclass.com>
2025-11-23 14:42:43 -0300

    Resend eofs

--- worker/common/joiner/by_user_store_id_joiner.go
+++ worker/common/joiner/by_user_store_id_joiner.go
@@ -41,6 +41,7 @@ func CreateByUserStoreIdJoinerCallbackWithOutput(outChan chan string, neededEof 
 		log.Errorf("Error loading clients EOF count: %v", err)
 		return nil
 	}
+	ResendClientEofs(clientsEofCount, neededEof, outChan)
 	processedStores := make(map[string]map[string]string)
 	// processedStores := ProcessStoreIds(storeIdRows)
 	return func(consumeChannel middleware.ConsumeChannel, done chan error) {
